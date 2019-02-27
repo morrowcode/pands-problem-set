3ac2e61a8f20e90ee719542c87242bbcb5d6ef0a
morrowcode <colinamorrow@googlemail.com>
2019-02-27 22:24:45 +0000

    2nd draft, it works but a bit long winded with format addition so if you change the letter it will change the letter in the print out, good for checking if it works

---
+++ begins-with-t.py
@@ -0,0 +1,23 @@
+# Problem 2 - Outputs whether or not today is a day that begins with the letter T
+# Colin Morrow - 27/02/2019
+
+#date and time info imported
+from datetime import date
+
+#to set when it is
+now = date.today()
+
+#to define the day of the week as the word
+day = now.strftime ("%A")
+
+# To choose what letter to query
+tdat = ("T")
+
+#square brackets for where in the word to look - tdat for what to look for
+if day[0] in tdat:
+    print("Yes - today begins with a {}.".format(tdat))
+else:
+    print("No - today does not begin with a {}.".format(tdat))
+
+#Commented out - used to make sure I'm definately getting day info through
+#print (day)
